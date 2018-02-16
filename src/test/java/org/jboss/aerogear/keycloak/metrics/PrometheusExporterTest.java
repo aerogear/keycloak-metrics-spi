d630897579e8dcda19815e0782e2554b4ca9d157
Ali Ok <aliok@apache.org>
2018-02-16 14:14:25 +0300

    AEROGEAR-2096 Added tests for different login cases and register cases

--- src/test/java/org/jboss/aerogear/keycloak/metrics/PrometheusExporterTest.java
+++ src/test/java/org/jboss/aerogear/keycloak/metrics/PrometheusExporterTest.java
@@ -47,24 +47,24 @@ public class PrometheusExporterTest {
         details1.put("identity_provider", "THE_ID_PROVIDER");
         final Event login1 = createEvent(EventType.LOGIN, details1);
         PrometheusExporter.instance().recordLogin(login1);
-        assertLoggedInCount(1, "THE_ID_PROVIDER");
+        assertMetric("keycloak_logins", "THE_ID_PROVIDER", 1);
 
         final Map<String, String> details2 = new HashMap<>();
         details2.put("identity_provider", "THE_ID_PROVIDER");
         final Event login2 = createEvent(EventType.LOGIN, details2);
         PrometheusExporter.instance().recordLogin(login2);
-        assertLoggedInCount(2, "THE_ID_PROVIDER");
+        assertMetric("keycloak_logins", "THE_ID_PROVIDER", 2);
     }
 
     @Test
     public void shouldCorrectlyCountLoginWhenIdentityProviderIsNotDefined() throws IOException {
         final Event login1 = createEvent(EventType.LOGIN);
         PrometheusExporter.instance().recordLogin(login1);
-        assertLoggedInCount(1, "keycloak");
+        assertMetric("keycloak_logins", "keycloak", 1);
 
         final Event login2 = createEvent(EventType.LOGIN);
         PrometheusExporter.instance().recordLogin(login2);
-        assertLoggedInCount(2, "keycloak");
+        assertMetric("keycloak_logins", "keycloak", 2);
     }
 
     @Test
@@ -74,20 +74,54 @@ public class PrometheusExporterTest {
         details1.put("identity_provider", "THE_ID_PROVIDER");
         final Event login1 = createEvent(EventType.LOGIN, details1);
         PrometheusExporter.instance().recordLogin(login1);
-        assertLoggedInCount(1, "THE_ID_PROVIDER");
+        assertMetric("keycloak_logins", "THE_ID_PROVIDER", 1);
 
         // without id provider defined
         final Event login2 = createEvent(EventType.LOGIN);
         PrometheusExporter.instance().recordLogin(login2);
-        assertLoggedInCount(1, "keycloak");
-        assertLoggedInCount(1, "THE_ID_PROVIDER");
+        assertMetric("keycloak_logins", "keycloak", 1);
+        assertMetric("keycloak_logins", "THE_ID_PROVIDER", 1);
     }
 
-    private void assertLoggedInCount(double number, String provider) throws IOException {
+//    @Test
+//    public void shouldCorrectlyCountLoginError() throws IOException {
+//        // with id provider defined
+//        final Map<String, String> details1 = new HashMap<>();
+//        details1.put("identity_provider", "THE_ID_PROVIDER");
+//        final Event event1 = createEvent(EventType.LOGIN_ERROR, details1);
+//        event1.setError("user_not_found");
+//        PrometheusExporter.instance().recordLoginError(event1);
+//        assertMetric("keycloak_failed_login_attempts", "THE_ID_PROVIDER", 1);
+//
+//        // without id provider defined
+//        final Event event2 = createEvent(EventType.LOGIN_ERROR);
+//        PrometheusExporter.instance().recordLoginError(event2);
+//        event2.setError("user_not_found");
+//        assertMetric("keycloak_failed_login_attempts", "keycloak", 1);
+//        assertMetric("keycloak_failed_login_attempts", "THE_ID_PROVIDER", 1);
+//    }
+
+    @Test
+    public void shouldCorrectlyCountRegister() throws IOException {
+        // with id provider defined
+        final Map<String, String> details1 = new HashMap<>();
+        details1.put("identity_provider", "THE_ID_PROVIDER");
+        final Event event1 = createEvent(EventType.REGISTER, details1);
+        PrometheusExporter.instance().recordRegistration(event1);
+        assertMetric("keycloak_registrations", "THE_ID_PROVIDER", 1);
+
+        // without id provider defined
+        final Event event2 = createEvent(EventType.REGISTER);
+        PrometheusExporter.instance().recordRegistration(event2);
+        assertMetric("keycloak_registrations", "keycloak", 1);
+        assertMetric("keycloak_registrations", "THE_ID_PROVIDER", 1);
+    }
+
+    private void assertMetric(String metricName, String provider, double metricValue) throws IOException {
         try (ByteArrayOutputStream stream = new ByteArrayOutputStream()) {
             PrometheusExporter.instance().export(stream);
             String result = new String(stream.toByteArray());
-            MatcherAssert.assertThat(result, containsString("keycloak_logins{realm=\"myrealm\",provider=\"" + provider + "\",} " + number));
+            MatcherAssert.assertThat(result, containsString(metricName + "{realm=\"myrealm\",provider=\"" + provider + "\",} " + metricValue));
         }
     }
 
